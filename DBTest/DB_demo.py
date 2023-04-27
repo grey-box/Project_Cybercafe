220f2cb357b211dac9f6cbbb68ead34f9dc805b6
adkpk <adkpk_pc>
2023-04-27 09:16:22 +0800

    Improvements on python and website

--- DBTest/DB_demo.py
+++ DBTest/DB_demo.py
@@ -1,12 +1,20 @@
-#import psycopg2
-#import psycopg2.extras
-
-
-
 #!/usr/bin/python
 # encoding: utf-8
 
-import sqlite3
+# Note: This is a python3 demo to show one possible way that how we can try to communicate with the database using SQLite3.
+#       Make sure put network-test.db into the same working folder.
+#       It does not aimed to be a real program which can be used in the router.
+#       The main purpose is to show how we can use python to communicate with the database.
+
+import sqlite3, subprocess
+
+def execute_cmd(cmd):
+    try:
+        subprocess.check_output(cmd, shell=True)
+    except subprocess.CalledProcessError as e:
+        print("Error: %s" % e)
+        return False
+    return True
 
 def get_choice(a, b):
     return enter_int(a, b, "Enter your choice")
@@ -45,7 +53,58 @@ def enter_str():
             print("Invalid Input!", flush=True)
             continue
     return s
-    
+
+def allow_type_filter():
+    cmd1 = 'uci set wireless.@wifi-iface[0].macfilter = "allow"'
+    cmd2 = 'uci commit wireless'
+    cmd3 = 'wifi reload'
+    execute_cmd(cmd1)
+    execute_cmd(cmd2)
+    execute_cmd(cmd3)
+    print("Allow-type filter has been set!")
+
+def deny_type_filter():
+    cmd1 = 'uci set wireless.@wifi-iface[0].macfilter = "deny"'
+    cmd2 = 'uci commit wireless'
+    cmd3 = 'wifi reload'
+    execute_cmd(cmd1)
+    execute_cmd(cmd2)
+    execute_cmd(cmd3)
+    print("Deny-type filter has been set!")
+
+def add_mac_address():
+    mac = enter_str()
+    cmd1 = f'uci add_list wireless.@wifi-iface[0].maclist = "{mac}"'
+    cmd2 = 'uci commit wireless'
+    cmd3 = 'wifi reload'
+    execute_cmd(cmd1)
+    execute_cmd(cmd2)
+    execute_cmd(cmd3)
+    print(f"The MAC address {mac} has been added to the list!")
+
+def check_settings():
+    cmd = 'uci show wireless.@wifi-iface[0]'
+    output = subprocess.check_output(cmd, shell=True)
+    print(output.decode())
+
+def submenu():
+    while(True):
+        choice = show_menu(["Allow-type filter", "Deny-type filter", "Add MAC address", "Check settings", "Save and apply"])
+        if choice == 1:
+            allow_type_filter()
+        elif choice == 2:
+            deny_type_filter()
+        elif choice == 3:
+            add_mac_address()
+        elif choice == 4:
+            check_settings()
+        elif choice == 5:
+            cmd = 'uci commit wireless && wifi reload'
+            execute_cmd(cmd)
+            print("Settings have been saved and applied!")
+        else:
+            print("Invalid choice!", flush=True)
+
 print("This is a demo to show one possible way that how we can try to communicate with the database.")
 print("Make sure put network-test.db into the same working folder.\n")
 try:
@@ -54,7 +113,7 @@ try:
         c = conn.cursor()
         i = -1
         while(i != 4):
-            i = show_menu("Show records,Add a new record,Delete a record,Exit".split(","))
+            i = show_menu("Show records,Add a new record,Delete a record,Show Submenu,xit".split(","))
             if(i == 1):
                 cursor = c.execute("SELECT * FROM RECORD")
                 print("\n"*2+" "*4+" |".join("%16s" % x for x in "Device ID, User name, Speed Limit, Data used, Data Limit".split(",")))
@@ -80,12 +139,11 @@ try:
                 d_id = enter_int(10000, 99999)
                 c.execute("DELETE FROM RECORD WHERE Device_ID=%d;" % d_id)
                 print("Done.")
+            elif(i == 4):
+                submenu()
         print("Exiting...")        
 except KeyboardInterrupt:
     print("Exit...")
-
-
-
 
 def search_address(s):
     try:
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Project-Cybercafe
 This is the repository for the Cybercafe Project.
+It is incomplete due to the time constraints of the project.
+The main purpose of this repository is to show the work we have done so far.
+
