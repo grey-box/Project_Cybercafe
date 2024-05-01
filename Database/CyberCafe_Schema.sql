b437389c2dcb08bc4bc7228db035b842313d65be
jvbyrnes <jackvbyrnes@gmail.com>
2024-05-01 18:51:15 +1000

    Implemented the function that updates data usage.

--- Database/CyberCafe_Schema.sql
+++ Database/CyberCafe_Schema.sql
@@ -1,6 +1,7 @@
 -- Mapping of session code to download limit and website blocking group.
 -- Used to initialise a session 
 -- Session code is entered by the user in the captive portal.
+DROP TABLE IF EXISTS session_types;
 CREATE TABLE session_types(
 	session_code TEXT NOT NULL,
 	group_id TEXT NOT NULL,
@@ -10,18 +11,19 @@ CREATE TABLE session_types(
 );
 
 -- Mapping session_id to a mac address (device) and a blocking group.
+DROP TABLE IF EXISTS session_details;
 CREATE TABLE session_details(
-	session_id INTEGER PRIMARY_KEY AUTOINCREMENT,
+	session_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	session_start TEXT NOT NULL,
 	session_end TEXT,
 	group_id TEXT NOT NULL,
 	mac_address TEXT NOT NULL,
-	bytes_remaining INTEGER DEFAULT 0,
-	PRIMARY KEY (session_id, mac_address),
+	bytes_remaining INTEGER NOT NULL,
 	FOREIGN KEY (group_id) REFERENCES website_blocking_groups(group_id)
 );
 
 -- Website blocking groups
+DROP TABLE IF EXISTS website_blocking_groups;
 CREATE TABLE website_blocking_groups (
 	group_id TEXT NOT NULL,
 	group_name TEXT,
@@ -29,6 +31,7 @@ CREATE TABLE website_blocking_groups (
 );
 
 -- Maps website blocking groups to URL
+DROP TABLE IF EXISTS website_blocking_groups_url;
 CREATE TABLE website_blocking_groups_url(
    website_url TEXT NOT NULL,
    group_id TEXT NOT NULL,
@@ -37,6 +40,7 @@ CREATE TABLE website_blocking_groups_url(
 );
 
 -- Admin users
+DROP TABLE IF EXISTS admin;
 CREATE TABLE admin(
 	username TEXT NOT NULL,
 	password TEXT NOT NULL
