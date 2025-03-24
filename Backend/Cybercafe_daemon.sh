e71f6416e29ec4b62ff37abb1e16c5d7a29ee079
Grey-box-johnfischer <John.Fischer@grey-box.ca>
2025-03-24 00:36:39 -0700

    removed some echo lines
    removed echo lines since daemon will now mostly be called from command line
    also uncommented clear_internet_sessions so that shutdown will clear internet sessions

--- Backend/Cybercafe_daemon.sh
+++ Backend/Cybercafe_daemon.sh
@@ -15,32 +15,25 @@ while true; do
 
 	#Case 1: Hotspot up and was up in previous check
     if [[ $HS_STATUS == 'up' ]] && [[ $HS_STATUS_PREV == 'up' ]]; then
-        printf "%s" "$(date +%T)" \
-        && echo ": Status normal"
 		check_internet_sessions
 	
 	#Case 2: Hotspot up and was down in previous check
     elif [[ $HS_STATUS == 'up' ]] && [[ $HS_STATUS_PREV == 'down' ]]; then
-        printf "%s" "$(date +%T)" \
-        && echo ": Hotspot up"
         setup_infrastructure
 	
 	#Case 3: Hotspot down and was up in previous check
     elif [[ $HS_STATUS == 'down' ]] && [[ $HS_STATUS_PREV == 'up' ]]; then
         # Assume hotspot has recently gone down. Clean up.
-        printf "%s" "$(date +%T)" \
-        && echo ": Hotspot went down"
 		clear_internet_sessions
         shutdown_infrastructure
 		
 	#Case 4: Hotspot down and was down in previous check
     else
-        printf "%s" "$(date +%T)" \
-        && echo ": Hotspot down"
+		:
     fi
 
 	if [[ -f ./shutdown.confirmed ]]; then
-		#clear_internet_sessions
+		clear_internet_sessions
 		shutdown_infrastructure
 		exit
 	fi
