08b40093c3ff9d0283d6fafe74436e5b7e1d9ebe
Grey-box-johnfischer <John.Fischer@grey-box.ca>
2025-03-24 00:41:59 -0700

    made /home the landing page

--- Website_ASU2024/page.php
+++ Website_ASU2024/page.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <title>Cybercafe Demo</title>
-</head>
-<body>
-	<h2>Greybox - Cybercafe</h2>
-        <a href="./login/">Login Page</a><br>
-		<a href="./createaccount/">Create Account</a><br>
-		<a href="./about/">About</a><br>
-</body>
-</html>
+<?php
+header('Location: /home');
+?>
