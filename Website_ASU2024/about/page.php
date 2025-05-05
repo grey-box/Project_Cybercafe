5ca8f4551139f87230655b756fd83ce7dd063a9e
Grey-box-johnfischer <John.Fischer@grey-box.ca>
2025-05-05 01:36:37 -0700

    Add files via upload

--- Website_ASU2024/about/page.php
+++ Website_ASU2024/about/page.php
@@ -2,7 +2,13 @@
 ini_set('display_errors', '0');
 ini_set('display_startup_errors', '0');
 #error_reporting(E_ALL);
-require __DIR__ . '/../globalfunctions.php';
+require __DIR__ . '/../global.php';
+$GLOBALS['aboutPageContent']='
+		<p>Project Cybercafe is software that runs on an Android device, enabling smart hotspot functionality for the sharing of internet connectivity 
+		with other Wi-Fi devices. Recognizing costs and other restrictions that may exist in our targeted regions, several controls are in place to allow 
+		a high level of control over how a user’s hotspot device is used for internet access.<br>
+		<a href="https://www.grey-box.ca/project-cybercafe/" target="_blank">https://www.grey-box.ca/project-cybercafe/</a></p>
+		';
 
 function aboutPageAdmin()
 {
@@ -15,31 +21,13 @@ function aboutPageAdmin()
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<style>
-	ul {
-		list-style-type: none;
-		margin: 0;
-		padding: 0;
-		overflow: hidden;
-		background-color: #e7e7e7;
-	}
-	li {
-		float: left;
-	}
-	li a {
-		display: block;
-		color: black;
-		text-align: center;
-		padding: 14px 16px;
-		text-decoration: none;
-	}
-	li a:hover {
-		background-color: #bfbfbf;
-	}
+	'.$GLOBALS['defaultStyle'].'
 	</style>
 	<body>
 		<a><img src="/assets/CyberCafe_logo.png" width="100" height="100"></a>
 		'.$GLOBALS['adminNavHTML'].'
 		<h2>About<h2>
+		'.$GLOBALS['aboutPageContent'].'
 	</body>
 	</html>
 	';
@@ -55,31 +43,13 @@ function aboutPageUser()
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<style>
-	ul {
-		list-style-type: none;
-		margin: 0;
-		padding: 0;
-		overflow: hidden;
-		background-color: #e7e7e7;
-	}
-	li {
-		float: left;
-	}
-	li a {
-		display: block;
-		color: black;
-		text-align: center;
-		padding: 14px 16px;
-		text-decoration: none;
-	}
-	li a:hover {
-		background-color: #bfbfbf;
-	}
+	'.$GLOBALS['defaultStyle'].'
 	</style>
 	<body>
 		<a><img src="/assets/CyberCafe_logo.png" width="100" height="100"></a>
 		'.$GLOBALS['userNavHTML'].'
 		<h2>About<h2>
+		'.$GLOBALS['aboutPageContent'].'
 	</body>
 	</html>
 	';
@@ -95,31 +65,13 @@ function aboutPageDefault()
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<style>
-	ul {
-		list-style-type: none;
-		margin: 0;
-		padding: 0;
-		overflow: hidden;
-		background-color: #e7e7e7;
-	}
-	li {
-		float: left;
-	}
-	li a {
-		display: block;
-		color: black;
-		text-align: center;
-		padding: 14px 16px;
-		text-decoration: none;
-	}
-	li a:hover {
-		background-color: #bfbfbf;
-	}
+	'.$GLOBALS['defaultStyle'].'
 	</style>
 	<body>
 		<a><img src="/assets/CyberCafe_logo.png" width="100" height="100"></a>
 		'.$GLOBALS['defaultNavHTML'].'
 		<h2>About<h2>
+		'.$GLOBALS['aboutPageContent'].'
 	</body>
 	</html>
 	';
